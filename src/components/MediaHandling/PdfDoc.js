48bee7df96eb95db66bd218f530414a2ba4d8455
afrizal0811 <afrizalmaulana0811@gmail.com>
2022-11-12 10:22:00 +0700

    edit PDF Document

--- src/components/MediaHandling/PdfDoc.js
+++ src/components/MediaHandling/PdfDoc.js
@@ -15,33 +15,50 @@ import Cookies from "js-cookie";
 const styles = StyleSheet.create({
   page: { backgroundColor: "#f1f3ff" },
   section: { textAlign: "center", margin: 30 },
-  judul: { textAlign: "center" },
-  informasi: { margin: 30 },
+  judul: { textAlign: "center", marginBottom: 30 },
+  informasi: {
+    marginLeft: 30,
+    marginTop: 30,
+    marginRight: 20,
+  },
+  infoWrapper: { flexDirection: "row" },
   image: { width: "200" },
   title: {
     fontSize: 43,
     textAlign: "center",
     fontWeight: "700",
-    marginBottom: "10",
+    marginBottom: 10,
   },
   address: {
     fontSize: 13,
-    marginBottom: "10",
+    marginBottom: 10,
   },
   subtitle: {
     fontSize: 26,
     textAlign: "center",
     fontWeight: "700",
-    marginBottom: "10",
+    marginBottom: 10,
   },
   order: {
     fontSize: 14,
     textAlign: "center",
   },
   info: {
-    fontSize: 20,
-    marginLeft: 20,
-    marginTop:10
+    fontSize: 16,
+    // marginLeft: 20,
+    marginTop: 15,
+  },
+  judulTotal: {
+    fontSize: 16,
+    marginRight: 25,
+    marginLeft: "auto",
+    marginTop: 80,
+  },
+  total: {
+    fontSize: 25,
+    marginRight: 25,
+    marginLeft: "auto",
+    marginTop: 15,
   },
 });
 const orderId = Cookies.get("order");
@@ -49,6 +66,9 @@ const mobil = Cookies.get("mobil");
 const startDate = Cookies.get("startDate");
 const endDate = Cookies.get("endDate");
 const lamaSewa = Cookies.get("lama");
+const details = localStorage.getItem("userDetails");
+const user = JSON.parse(details);
+
 let harga = "Rp" + new Intl.NumberFormat("id").format(Cookies.get("harga"));
 // Create Document Component
 const PdfDoc = () => (
@@ -64,20 +84,27 @@ const PdfDoc = () => (
         <Svg height="5" width="550">
           <Line x1="10" y1="0" x2="520" y2="0" strokeWidth={5} stroke="black" />
         </Svg>
-        <Text>
-          <hr />
-        </Text>
+        <Text></Text>
       </View>
       <View style={styles.judul}>
         <Text style={styles.subtitle}>Invoice Sewa Mobil</Text>
         <Text style={styles.order}>Order ID: {orderId}</Text>
       </View>
-      <View style={styles.informasi}>
-        <Text style={styles.info}>Nama Mobil : {mobil}</Text>
-        <Text style={styles.info}>Tanggal Sewa : {startDate}</Text>
-        <Text style={styles.info}>Tanggal Kembali : {endDate}</Text>
-        <Text style={styles.info}>Lama Sewa : {lamaSewa} hari</Text>
-        <Text style={styles.info}>Harga Sewa : {harga}</Text>
+      <View style={styles.infoWrapper}>
+        <View style={styles.informasi}>
+          <Text style={styles.info}>Email Penyewa : {user.email}</Text>
+          <Text style={styles.info}>Nama Penyewa : {user.fullname}</Text>
+          <Text style={styles.info}>Nama Mobil : {mobil}</Text>
+        </View>
+        <View style={styles.informasi}>
+          <Text style={styles.info}>Tanggal Sewa : {startDate}</Text>
+          <Text style={styles.info}>Tanggal Kembali : {endDate}</Text>
+          <Text style={styles.info}>Lama Sewa : {lamaSewa} hari</Text>
+        </View>
+      </View>
+      <View style={styles.total}>
+        <Text style={styles.judulTotal}>Total Pembayaran</Text>
+        <Text style={styles.total}>{harga}</Text>
       </View>
     </Page>
   </Document>
