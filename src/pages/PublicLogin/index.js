c10d1d349f821969678b1c117d75cb7d789a73a3
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-03-19 16:48:23 +0700

    edit sign-in/up page

--- src/pages/PublicLogin/index.js
+++ src/pages/PublicLogin/index.js
@@ -1,101 +1,40 @@
-import React from "react";
-import "./index.css";
-import { Alert } from "react-bootstrap";
-import imagePath from "../../constants/imagePath";
-import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Handlebutton from "../../components/LoginLogic/handlebutton";
-import { useLocation } from "react-router";
-import queryString from "query-string";
-import Cookies from "js-cookie";
+import React from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import imagePath from '../../constants/imagePath'
+import './index.css'
 
-const PublicLogin = (submit) => {
-  const isRegis = Cookies.get("isRegis");
-  const location = useLocation();
-  const queries = queryString.parse(location.search);
-
-  const { handleChange, handleSubmit, errors, values, submitted } =
-    Handlebutton(submit, queries);
+const PublicLogin = () => {
+  const formGroup = (label, type, placeholder, name) => (
+    <Form.Group
+      className='mb-3'
+      controlId='formBasicEmail'
+    >
+      <Form.Label>{label}</Form.Label>{' '}
+      <Form.Control
+        type={type}
+        placeholder={placeholder}
+        name={name}
+      />
+    </Form.Group>
+  )
+  
   return (
     <section className='sign-section'>
       <div className='sign-form'>
-        {isRegis && (
-          <div className='status-error'>
-            <Alert
-              variant='success'
-              style={{ textAlign: 'center' }}
-            >
-              Registration Success! Please login.
-            </Alert>
-          </div>
-        )}
-        {errors.status && (
-          <div className='status-error'>
-            <Alert
-              variant='danger'
-              style={{ textAlign: 'center' }}
-            >
-              {errors.status}
-            </Alert>
-          </div>
-        )}
         <div className='square'></div>
         <h1>Welcome Back!</h1>
         <Form>
-          <Form.Group
-            className='mb-3'
-            controlId='formBasicEmail'
-          >
-            <Form.Label>Email</Form.Label>{' '}
-            {errors.email && (
-              <span style={{ color: 'red' }}>&#42;{errors.email}</span>
-            )}
-            <Form.Control
-              type='email'
-              placeholder='Contoh: johndee@gmail.com'
-              onChange={handleChange}
-              value={values.email}
-              name='email'
-            />
-          </Form.Group>
-
-          <Form.Group
-            className='mb-3'
-            controlId='formBasicPassword'
-          >
-            <Form.Label>Password</Form.Label>{' '}
-            {errors.password && (
-              <span style={{ color: 'red' }}>&#42;{errors.password}</span>
-            )}
-            <Form.Control
-              type='password'
-              placeholder='6+ Password'
-              onChange={handleChange}
-              value={values.password}
-              name='password'
-            />
-          </Form.Group>
+          {formGroup('Email', 'email', 'Contoh: johndee@gmail.com', 'email')}
+          {formGroup('Password', 'password', '6+ Password', 'password')}
           <div className='d-grid gap-2'>
-            {!submitted && (
-              <Button
-                variant='primary'
-                type='submit'
-                className='btn-submit'
-                onClick={handleSubmit}
-              >
-                Sign In
-              </Button>
-            )}
-            {submitted && (
-              <Button
-                variant='primary'
-                type='submit'
-                className='btn-submit'
-                onClick={handleSubmit}
-              >
-                Sign In
-              </Button>
-            )}
+            <Button
+              variant='primary'
+              type='submit'
+              className='btn-submit'
+            >
+              Sign In
+            </Button>
           </div>
           <p>
             Don&apos;t have an account?{' '}
@@ -115,6 +54,6 @@ const PublicLogin = (submit) => {
       />
     </section>
   )
-};
+}
 
-export default PublicLogin;
+export default PublicLogin
--- src/pages/PublicRegister/index.js
+++ src/pages/PublicRegister/index.js
@@ -1,87 +1,38 @@
-import React from "react";
-import "./index.css";
-import imagePath from "../../constants/imagePath";
-import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Alert } from "react-bootstrap";
-import Handlebutton from "../../components/registerlogic/Handlebutton";
+import React from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import imagePath from '../../constants/imagePath'
+import './index.css'
 
-const PublicRegister = (submitForm) => {
-  const { handleChange, handleSubmit, errors, value, submitted } =
-    Handlebutton(submitForm);
+const PublicRegister = () => {
+  const formGroup = (label, type, placeholder, name) => (
+    <Form.Group
+      className='mb-3'
+      controlId='formBasicEmail'
+    >
+      <Form.Label>{label}</Form.Label>{' '}
+      <Form.Control
+        type={type}
+        placeholder={placeholder}
+        name={name}
+      />
+    </Form.Group>
+  )
 
   return (
     <section className='sign-section'>
       <div className='sign-form'>
-        {errors.status && (
-          <div className='status-error'>
-            <Alert
-              variant='danger'
-              style={{ textAlign: 'center' }}
-            >
-              {errors.status}
-            </Alert>
-          </div>
-        )}
         <div className='square'></div>
         <h1>Sign Up</h1>
         <Form>
-          <Form.Group
-            className='mb-3'
-            controlId='formBasicEmail'
-          >
-            <Form.Label>Email</Form.Label>{' '}
-            {errors.email && (
-              <span style={{ color: 'red' }}>&#42;{errors.email}</span>
-            )}
-            <Form.Control
-              data-testid='form1'
-              type='email'
-              placeholder='Contoh: johndee@gmail.com'
-              value={value.email}
-              name='email'
-              onChange={handleChange}
-            />
-          </Form.Group>
-
-          <Form.Group
-            className='mb-3'
-            controlId='formBasicPassword'
-          >
-            <Form.Label>Create Password</Form.Label>{' '}
-            {errors.password1 && (
-              <span style={{ color: 'red' }}>&#42;{errors.password1}</span>
-            )}
-            <Form.Control
-              type='password'
-              placeholder='6+ Password'
-              name='password1'
-              value={value.password1}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group
-            className='mb-3'
-            controlId='formBasicPassword'
-          >
-            <Form.Label>Confirm Password</Form.Label>{' '}
-            {errors.password2 && (
-              <span style={{ color: 'red' }}>&#42;{errors.password2}</span>
-            )}
-            <Form.Control
-              type='password'
-              placeholder='6+ Password'
-              name='password2'
-              value={value.password2}
-              onChange={handleChange}
-            />
-          </Form.Group>
+          {formGroup('Email', 'email', 'Contoh: johndee@gmail.com', 'email')}
+          {formGroup('Create Password', 'password', '6+ Password', 'password')}
+          {formGroup('Confirm Password', 'password', '6+ Password', 'password')}
           <div className='d-grid gap-2'>
             <Button
               variant='primary'
               type='submit'
               className='btn-submit'
-              onClick={handleSubmit}
             >
               Sign Up
             </Button>
@@ -104,6 +55,6 @@ const PublicRegister = (submitForm) => {
       />
     </section>
   )
-};
+}
 
-export default PublicRegister;
+export default PublicRegister
