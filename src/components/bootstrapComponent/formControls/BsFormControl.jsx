7005c3e2064ab81238bc5ad2fc08c3ef8eff48a8
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-05-26 21:04:08 +0700

    add visibility password

--- src/components/bootstrapComponent/formControls/BsFormControl.jsx
+++ src/components/bootstrapComponent/formControls/BsFormControl.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
 import { Form } from 'react-bootstrap'
+import './index.css'
 
 const BsFormControl = (props) => {
   const {
@@ -12,24 +15,49 @@ const BsFormControl = (props) => {
     type,
     minLength,
   } = props
+  const [isVisible, setIsVisible] = useState({
+    password: false,
+    confirmPassword: false,
+  })
+
+  const handleOnClick = () => {
+    setIsVisible((prev) => ({
+      ...prev,
+      [name]: !isVisible[name],
+    }))
+  }
 
   const feedbackComp = (
     <Form.Control.Feedback type='invalid'>{isInvalid}</Form.Control.Feedback>
   )
-
   const renderFeedback = isInvalid ? feedbackComp : null
+  const visibleIcon = (
+    <a
+      onClick={handleOnClick}
+      style={{ cursor: 'pointer' }}
+      className='visible-icon'
+    >
+      <FontAwesomeIcon icon={isVisible[name] ? faEye : faEyeSlash} />
+    </a>
+  )
+
+  const isTypePassword = type === 'password'
+
   return (
     <div>
-      <Form.Control
-        autoComplete={autoComplete}
-        isInvalid={isInvalid}
-        minLength={minLength}
-        name={name}
-        onChange={onChange}
-        placeholder={placeholder}
-        required={required}
-        type={type}
-      />
+      <div className='password-container '>
+        <Form.Control
+          autoComplete={autoComplete}
+          isInvalid={isInvalid}
+          minLength={minLength}
+          name={name}
+          onChange={onChange}
+          placeholder={placeholder}
+          required={required}
+          type={!isVisible[`${name}`] ? 'password' : 'text'}
+        />
+        {isTypePassword && visibleIcon}
+      </div>
       {renderFeedback}
     </div>
   )
