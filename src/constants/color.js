42c49874891ddb7e190c2ab9e3e9076c3d05cc6e
afrizal0811 <afrizalmaulana0811@gmail.com>
2024-09-10 09:55:45 +0700

    edit styling rent page

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,4 +1,5 @@
 const color = {
   aliceBlue: `#F1F3FF`,
+  egyptianBlue: '#0D28A6',
 }
 export default color
--- src/pages/landing_page/rent/Rent.jsx
+++ src/pages/landing_page/rent/Rent.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import { useOutletContext } from 'react-router-dom'
 import { Button } from '../../../components/bootstrapComponent'
-import './Styled.css'
+import color from '../../../constants/color'
+
 const Rent = () => {
   const { navigate } = useOutletContext()
+  const renderText = (className) => (
+    <p className={`py-2 px-4 fs-6 fw-bolder ${className}`}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet odio
+      blanditiis voluptatem libero sit quam nisi deserunt quas, exercitationem,
+      rem porro quia atque aspernatur cum, ipsum nemo nihil vel sed.
+    </p>
+  )
+
   return (
-    <section className='sewa-section'>
-      <div className='sewa-container'>
-        <h1>Sewa Mobil di Jawa Timur Sekarang</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet odio
-          blanditiis voluptatem libero sit quam nisi deserunt quas,
-          exercitationem, rem porro quia atque aspernatur cum, ipsum nemo nihil
-          vel sed.
-        </p>
+    <section className='w-100 h-auto p-2'>
+      <div
+        className='d-flex flex-column justify-content-center align-items-center text-white rounded mx-2 mx-lg-5 my-lg-3 text-center '
+        style={{ backgroundColor: color.egyptianBlue, height: '500px' }}
+      >
+        <h1 className='py-2 px-4 fs-1 fw-bolder'>
+          Sewa Mobil di Jawa Timur Sekarang
+        </h1>
+        {renderText('w-75 d-md-none')}
+        {renderText('d-none d-md-flex flex-column w-50')}
         <Button
           variant='success'
           onClick={() => navigate(`/cars`)}
